e4d3461d3748aa00f3928d69d7be5e94e70a0cf7
yves848 <yves.godart@gmail.com>
2023-03-01 10:16:04 +0100

    Initial commit

---
+++ prepare.ps1
@@ -0,0 +1,11 @@
+﻿
+[console]::Clear();
+$width = $host.UI.RawUI.WindowSize.Width
+Write-Host "".PadLeft($Width, "~")
+Write-Host " Décompression des archives ....."
+Expand-Archive -Path .\export_pharmonizer_712901_1673885076.zip . 
+Expand-Archive -Path .\export_pharmonizer_extra_712901_1673885116.zip .
+Write-Host " => 👍"
+
+
+
